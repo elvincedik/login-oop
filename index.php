4de1541925379e202d770c596c7dfac600a40a52
elvincedik <elvinse3@gmail.com>
2024-12-05 09:52:28 +0100

    #

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
     session_start();
     $errors = isset($_SESSION["errors"]) ? $_SESSION["errors"] : [];
-    $old = isset($_SESSION["old"]) ? $_SESSION["old"] : [];
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,12 +8,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Sign Up</title>
-    <!-- <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
-      crossorigin="anonymous"
-    /> -->
+
     <link rel="stylesheet" href="./assets/css/bootstrap.min.css" />
     <link rel="stylesheet" href="./assets/css/style2.css" />
   </head>
@@ -152,11 +146,7 @@
           </form>
           
 
-    <!-- <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-      crossorigin="anonymous"
-    ></script> -->
+
     <script src="./assets/js/bootstrap.bundle.min.js"></script>
 
     <?php
