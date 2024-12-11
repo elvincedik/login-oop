1078e76d5da424ff825e0f379a5df3a7cf05d246
elvincedik <elvinse3@gmail.com>
2024-12-11 18:57:47 +0100

    implementing updated code for login-oop

--- process.php
+++ process.php
@@ -5,16 +5,11 @@ require 'validator.php';
 if($_SERVER["REQUEST_METHOD"] == "POST") {
 
     function purgeData($data) {
-        $data = stripslashes($_POST["fullName"]);
+        $data = stripslashes($data);
         $data = htmlspecialchars($data);
         $data = trim($data);
         return $data;
     }
-
-    echo purgeData($_POST["fullName"])."<br>";
-    echo purgeData($_POST["email"])."<br>";
-    echo purgeData($_POST["pwd"])."<br>";
-    echo purgeData($_POST["confirmPwd"])."<br>";
 
     // creating an object from a class
     $validator = new validator();
